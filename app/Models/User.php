4d895b6c84dbe702b43c64e9c6fed43d04e603e4
christopher-alden <christopheralden04@gmail.com>
2024-11-25 16:10:02 +0700

    chill guy

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,20 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    protected $fillable = ['name', 'email', 'password', 'phoneNumber', 'role', 'link', 'jobs'];
-    protected $hidden = ['password', 'remember_token'];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'phoneNumber',
+        'role',
+        'link',
+        'jobs'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
 
     protected function casts(): array
     {
